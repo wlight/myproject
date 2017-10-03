549b287b46b188525564ea4336ccc4cb14e50c01
wlight <wormside@gmail.com>
2017-10-03 15:00:19 +0000

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -22,13 +23,15 @@ class UsersController extends Controller
       $this->validate($request, [
         'name' => 'required|max:50',
         'email' => 'required|email|unique:users|max:255',
-        'password' => 'required|confirmed'
+        'password' => 'required|confirmed|min:6'
       ]);
       $user = User::create([
         'name' => $request->name,
         'email' => $request->email,
         'password' => bcrypt($request->password)
       ]);
+
+      Auth::login($user);
       session()->flash('success', '欢迎，您将在这里开始一段新的旅程~');
       return redirect()->route('users.show', [$user]);
     }
